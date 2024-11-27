9af9d82cc76f28fa1c0690c0bc99ea4df1d91337
ManoloMoraes <61664665+ManoloMoraes@users.noreply.github.com>
2024-11-27 01:16:50 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Renata, o Felipe ama você, se vc ama ele dá uma risadinha!</h1>
+    return <h1>Renata, o Felipe ama você, se vc ama ela dá uma risadinha! kkkk </h1>
 }
 
 export default Home;
