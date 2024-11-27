25db761a11b09ad1727cded3454fc876d6446a2c
ManoloMoraes <61664665+ManoloMoraes@users.noreply.github.com>
2024-11-27 17:06:03 +0000

    adiciona scripts `lint:check` e `lin:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Renata, o Felipe ama você, se vc ama ela dá uma risadinha! kkkk </h1>
+  return (
+    <h1>Renata, o Felipe ama você, se vc ama ela dá uma risadinha! kkkk </h1>
+  );
 }
 
-export default Home;
+default Home;
