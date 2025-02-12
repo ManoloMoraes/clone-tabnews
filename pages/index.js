d620cce543847e72b645b2d5de7aaa9ed445339e
ManoloMoraes <61664665+ManoloMoraes@users.noreply.github.com>
2025-02-12 14:01:31 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <h1>
+      “No mundo tereis aflição, mas tende bom ânimo, eu venci o mundo” (João
+      16:33){" "}
+      <p>
+        <h2>
+          Mana, oro sempre pra que Deus ilumine sua mente para entender e
+          aceitar nossa total dependência dele, somos apenas peregrinos nesse
+          mundo."
+        </h2>
+      </p>
+    </h1>
+  );
 }
 
 export default Home;
